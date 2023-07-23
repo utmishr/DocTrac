08db91885427eb9e77f65dd4728c47382acd39c2
utmishr <96099759+utmishr@users.noreply.github.com>
2023-07-24 00:25:34 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+require('express-async-errors');
+const dotenv = require('dotenv');
+require('dotenv').config()
+const routes = require('./router/index');
+const db = require('./config/localdb');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+// Parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+// Parse URL-encoded bodies
+
+
+app.use(morgan('dev'));
+app.use((err, req, res, next) => {
+    if (err.message === 'access denied') {
+      res.status(401);
+      res.json({ error: err.message });
+    }
+});
+app.use('/',routes);
+const port = 8000;
+console.log(process.env.api_key, process.env.api_secret, process.env.cloud_name);
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+// q:adding remote link to this project
+// q: git push 
+
+
